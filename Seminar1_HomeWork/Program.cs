a785094d8e8c67319edefd9d2f425728655d40a9
Vadim <ivm99@mail.ru>
2022-06-07 22:01:16 +0300

    Task 2 solved

--- Seminar1_HomeWork/Program.cs
+++ Seminar1_HomeWork/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задача №1
 
-Console.Write("Input first number: ");
+/*Console.Write("Input first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
@@ -14,4 +14,23 @@ else
 {
     Console.WriteLine("max = " + num2);
     Console.WriteLine("min = " + num1);
-}
+}*/
+
+//Задача №2
+
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third number:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+
+if (num3 > max) max = num3;
+
+Console.WriteLine("max = " + max);
