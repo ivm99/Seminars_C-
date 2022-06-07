867da4888686315b8d828ba0c7e7a84e5831f557
Vadim <ivm99@mail.ru>
2022-06-07 21:59:52 +0300

    Task 1 solved

---
+++ Seminar1_HomeWork/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача №1
+
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("max = " + num1);
+    Console.WriteLine("min = " + num2);
+}
+else
+{
+    Console.WriteLine("max = " + num2);
+    Console.WriteLine("min = " + num1);
+}
